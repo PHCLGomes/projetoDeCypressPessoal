1e88b0b5536faa3b74708013531178318a171dd7
Pedro Henrique <122324968+PHCLGomes@users.noreply.github.com>
2023-07-30 23:19:43 -0300

    Commit do dia 30/07/23 - 23:20

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
 	e2e: {
 		baseUrl:'https://notes-serverless-app.com',
+		env: {viewportWidthBreakpoint: 768,},
 		defaultCommandTimeout: 15000,
 		requestTimeout: 10000,
 		chromeWebSecurity: false,
