44e40c5d057fafcf6aa5e5ae85179cea28ffea16
Pedro Henrique <122324968+PHCLGomes@users.noreply.github.com>
2023-08-06 21:07:11 -0300

    corrigindo teste

--- cypress.config.js
+++ cypress.config.js
@@ -6,10 +6,10 @@ module.exports = defineConfig({
 		env: {viewportWidthBreakpoint: 768,},
 		defaultCommandTimeout: 15000,
 		requestTimeout: 10000,
-		projectId: '48vhv2',
 		chromeWebSecurity: false,
 		setupNodeEvents() {
 			// implement node event listeners here
-		},
+		},	
 	},
+	projectId: '48vhv2',
 })
